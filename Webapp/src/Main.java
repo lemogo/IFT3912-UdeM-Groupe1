39dd37f8baf41320148697d457a6451e74f3d453
willy <ndjeup@gmail.com>
2014-02-24 20:45:31 -0500

    update1 database

--- Webapp/src/Main.java
+++ Webapp/src/Main.java
@@ -5,8 +5,7 @@ public class Main {
 	public static final int DEFAULT_PORT = 8080;
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(DEFAULT_PORT);
-		
-		server.setHandler(new HelloWorldHandler());
+		server.setHandler(new MustacheHandler());
 		server.start();
 		server.join();
 	}
